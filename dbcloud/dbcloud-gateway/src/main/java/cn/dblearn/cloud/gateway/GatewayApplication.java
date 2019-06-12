940aac3518b88edd7968d74c50cee3caecaa0440
llldddbbb <571002217@qq.com>
2019-06-12 18:49:17 +0800

    feat[gateway]:添加网关»件

---
+++ dbcloud/dbcloud-gateway/src/main/java/cn/dblearn/cloud/gateway/GatewayApplication.java
@@ -0,0 +1,21 @@
+package cn.dblearn.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * cn.dblearn.cloud.gateway.GatewayApplication
+ *
+ * @author bobbi
+ * @date 2019/06/11 12:33
+ * @email 571002217@qq.com
+ * @description
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+}
